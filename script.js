bf4ab778e3ed24429f39a80108229d6f6bb6fa83
Georgiana Dancu <geor1698@stud.kea.dk>
2019-06-14 13:46:07 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var coll = document.getElementById("btn");
+var i;
+
+
+let btn = document.querySelector("#btn")
+let contact = document.querySelector(".contact")
+let formSection = document.querySelector(".form-section")
+
+btn.addEventListener("click", function () {
+    contact.classList.add("show")
+    btn.classList.add("hide")
+    formSection.style.height="auto"
+});
